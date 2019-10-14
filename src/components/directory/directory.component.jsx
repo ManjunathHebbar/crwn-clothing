41bb570772d4a7248cbb54e822f4f933956a44d4
Manjunath Hebbar <hmanjunath192@gmail.com>
2019-10-14 14:26:47 +0530

    broken our homepage to smaller components,finished styling our menu-list

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import './directory/directory.styles.css';
-
+import './directory.styles.scss';
+import {MenuItem} from '../menu-item/menu.component'
 export class Directory extends React.Component{
  constructor(){
      super()
@@ -42,10 +42,10 @@ export class Directory extends React.Component{
     render(){
         return(
             <div className="directory-menu">
-                {this.state.sections.map(({id, title,imageUrl}) =>  (
-                <menuItem key={id} title={title} imageUrl={imageUrl}/>
+                {this.state.sections.map(({id,title,imageUrl,size}) => (
+                <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
                 ))}
             </div>
-        
+        );
     }
 }
